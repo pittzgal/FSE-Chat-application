0f51cb99112090f93711db39d5fe003cf2370e75
Ranjani Sankar <sivarans@andrew.cmu.edu>
2015-01-19 09:16:57 -0800

    small changes

--- app.js
+++ app.js
@@ -9,16 +9,9 @@ res.sendfile(__dirname+'/index.html');
 });
 
 io.sockets.on('connection', function (socket) {
-    //when recieving the data from the server, push the same message to client.
     socket.on("clientMsg", function (data) {
-        //send the data to the current client requested/sent.
-        socket.emit("serverMsg", data);
-        //send the data to all the clients who are acessing the same site(localhost)
-        socket.broadcast.emit("serverMsg", data);
+       socket.emit("serverMsg", data);
+       socket.broadcast.emit("serverMsg", data);
     });
 
-    socket.on("sender", function (data) {
-        socket.emit("sender", data);
-        socket.broadcast.emit("sender", data);
-    });
 });
