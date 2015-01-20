e783e607b0925ab78d253a7dd29d6d4f33c4d3f9
Ranjani Sankar <sivarans@andrew.cmu.edu>
2015-01-19 16:43:12 -0800

    added some extra stuff

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express=require('express'),
 app=express(),
 server=require('http').createServer(app),
 io=require('socket.io').listen(server);
-
+var name;
 server.listen(3000);
 app.get('/',function(req,res){
 res.sendfile(__dirname+'/index.html');
@@ -10,8 +10,13 @@ res.sendfile(__dirname+'/index.html');
 
 io.sockets.on('connection', function (socket) {
     socket.on("clientMsg", function (data) {
+       name=data.name;
        socket.emit("serverMsg", data);
        socket.broadcast.emit("serverMsg", data);
     });
 
+	socket.on('disconnect', function(data){
+	socket.broadcast.emit('left',name);
+			
+			});
 });
